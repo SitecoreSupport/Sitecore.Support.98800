9faa25877d19167cc34363fbac112ca1bc05d896
Paul Stupka <paul.stupka@sitecore.com>
2019-04-09 16:36:45 +0300

    clean solution

---
+++ src/Sitecore.Support.98800/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.98800")]
+[assembly: AssemblyProduct("Sitecore.Support.98800")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Inefficient processing of expired sessions")]
+
